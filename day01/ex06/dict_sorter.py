7315da20035f9da0a9adf33fab90bd7f13aea972
Smoooky <smoooky2012@gmail.com>
2022-09-18 12:42:08 +0300

    ex06 started

--- day01/ex06/dict_sorter.py
+++ day01/ex06/dict_sorter.py
@@ -1,36 +1,31 @@
-def print_dict() -> None:
-    list_of_tuples = [
-        ('Russia', '25'),
-        ('France', '132'),
-        ('Germany', '132'),
-        ('Spain', '178'),
-        ('Italy', '162'),
-        ('Portugal', '17'),
-        ('Finland', '3'),
-        ('Hungary', '2'),
-        ('The Netherlands', '28'),
-        ('The USA', '610'),
-        ('The United Kingdom', '95'),
-        ('China', '83'),
-        ('Iran', '76'),
-        ('Turkey', '65'),
-        ('Belgium', '34'),
-        ('Canada', '28'),
-        ('Switzerland', '26'),
-        ('Brazil', '25'),
-        ('Austria', '14'),
-        ('Israel', '12')
-    ]
-    dict = {}
-    for value, key in list_of_tuples:
-        if key not in  dict:
-            dict[key] = [value]
-        else:
-            dict[key].append(value)
-    dict.sort()
-    for key, value in dict.items():
-        for country in value:
-            print(f"'{key}' : '{country}'")
+def get_dict() -> dict:
+    country_codes = {
+        'Russia': '25',
+        'France': '132',
+        'Germany': '132',
+        'Spain': '178',
+        'Italy': '162',
+        'Portugal': '17',
+        'Finland': '3',
+        'Hungary': '2',
+        'The Netherlands': '28',
+        'The USA': '610',
+        'The United Kingdom': '95',
+        'China': '83',
+        'Iran': '76',
+        'Turkey': '65',
+        'Belgium': '34',
+        'Canada': '28',
+        'Switzerland': '26',
+        'Brazil': '25',
+        'Austria': '14',
+        'Israel': '12'
+    }
+    return  country_codes
+
+
+def sort_dict(country_codes: dict) -> dict:
+    return dict(sorted(country_codes.items(), key=lambda item: (-int(item[1]), item[0])))
 
 
 def main():
